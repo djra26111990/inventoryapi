e952531e13d4c8e733a220df37aff7ee2c5a1202
djra26111990 <djra26111990@gmail.com>
2020-06-11 17:37:22 -0500

    Fix mongoose watcher

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 //Se establece conexion con MongoDB en Atlas
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
